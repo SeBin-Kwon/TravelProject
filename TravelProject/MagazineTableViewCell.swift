fc9c17b17add2a9d728e85a40e498761140b9d32
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-04 02:15:43 +0900

    Add: Cell 추가 및 구조체 정의

---
+++ TravelProject/MagazineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MagazineTableViewCell.swift
+//  TravelProject
+//
+//  Created by Sebin Kwon on 1/4/25.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
