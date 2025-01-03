0dbd7d3b9eccb26b4a56265062ed120101ab9f26
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-04 02:42:36 +0900

    Add: Cell에 image 추가, Kingfisher 패키지 추가

--- TravelProject/MagazineTableViewCell.swift
+++ TravelProject/MagazineTableViewCell.swift
@@ -9,15 +9,17 @@ import UIKit
 
 class MagazineTableViewCell: UITableViewCell {
 
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- TravelProject/MagazineTableViewController.swift
+++ TravelProject/MagazineTableViewController.swift
@@ -6,20 +6,31 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MagazineTableViewController: UITableViewController {
 
+    let magazines = MagazineInfo().magazine
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 450
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return magazines.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath)
+        let magazineCell = cell as? MagazineTableViewCell
+        guard let magazineCell else { return cell }
+        
+        let magazine = magazines[indexPath.row]
+        guard let url = URL(string: magazine.photo_image) else { return cell }
+        magazineCell.photoImageView.kf.setImage(with: url)
+        magazineCell.photoImageView.contentMode = .scaleAspectFill
+        magazineCell.photoImageView.layer.cornerRadius = 15
         return cell
     }
 
