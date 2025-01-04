977141d3626d88d67669265f14d794b378755a23
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-04 23:57:32 +0900

    Update: configureCellUI로 함수 분리, 광고 데이터 배열 분리

--- TravelProject/TravelInfoTableViewController.swift
+++ TravelProject/TravelInfoTableViewController.swift
@@ -11,12 +11,21 @@ import Kingfisher
 class TravelInfoTableViewController: UITableViewController {
     
     @IBOutlet var navigationBar: UINavigationItem!
-    var travelInfo = TravelInfo().travel
+    let travelInfoList = TravelInfo().travel
+    var travelInfo = [Travel]()
+    var travelAd = [Travel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 134
         navigationBar.title = "도시 상세 정보"
+        travelInfoList.forEach {
+            if $0.ad {
+                travelAd.append($0)
+            } else {
+                travelInfo.append($0)
+            }
+        }
     }
     
     @objc
@@ -30,7 +39,6 @@ class TravelInfoTableViewController: UITableViewController {
         return travelInfo.count
     }
     
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TravelInfoTableViewCell", for: indexPath)
         guard let travelInfoCell = cell as? TravelInfoTableViewCell else { return cell }
@@ -39,31 +47,34 @@ class TravelInfoTableViewController: UITableViewController {
         let urlString = row.travel_image ?? ""
         guard let url = URL(string: urlString) else { return cell }
         travelInfoCell.travelImageView.kf.setImage(with: url)
-        travelInfoCell.travelImageView.layer.cornerRadius = 10
-        travelInfoCell.travelImageView.contentMode = .scaleAspectFill
-        
-        travelInfoCell.titleLabel.text = row.title
-        travelInfoCell.titleLabel.font = .systemFont(ofSize: 15, weight: .bold)
-        
-        travelInfoCell.descriptionLabel.text = row.description
-        travelInfoCell.descriptionLabel.font = .systemFont(ofSize: 13, weight: .medium)
-        travelInfoCell.descriptionLabel.textColor = .gray
-        travelInfoCell.descriptionLabel.numberOfLines = 0
-        
-        travelInfoCell.gradeLabel.text = "별점 \(String(row.grade ?? 0.0))"
-        travelInfoCell.gradeLabel.textColor = .systemGray2
-        travelInfoCell.gradeLabel.font = .systemFont(ofSize: 12)
-        
-        let save = row.save?.formatted()
-        travelInfoCell.saveLabel.text = " · 저장 \(save ?? "0")"
-        travelInfoCell.saveLabel.textColor = .systemGray2
-        travelInfoCell.saveLabel.font = .systemFont(ofSize: 12)
-        
+        configureCellUI(cell: travelInfoCell, row: row)
         travelInfoCell.likeButton.tag = indexPath.row
-        travelInfoCell.likeButton.tintColor = .white
-        travelInfoCell.likeButton.setImage(UIImage(systemName: row.like ?? false ? "heart.fill" : "heart"), for: .normal)
         travelInfoCell.likeButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
         
         return travelInfoCell
+    }
+    
+    func configureCellUI(cell: TravelInfoTableViewCell, row: Travel) {
+        cell.travelImageView.layer.cornerRadius = 10
+        cell.travelImageView.contentMode = .scaleAspectFill
+        
+        cell.titleLabel.text = row.title
+        cell.titleLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        
+        cell.descriptionLabel.text = row.description
+        cell.descriptionLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        cell.descriptionLabel.textColor = .gray
+        cell.descriptionLabel.numberOfLines = 0
+        
+        cell.gradeLabel.text = "별점 \(String(row.grade ?? 0.0))"
+        cell.gradeLabel.textColor = .systemGray2
+        cell.gradeLabel.font = .systemFont(ofSize: 12)
+        
+        cell.saveLabel.text = " · 저장 \(row.save?.formatted() ?? "0")"
+        cell.saveLabel.textColor = .systemGray2
+        cell.saveLabel.font = .systemFont(ofSize: 12)
+        
+        cell.likeButton.tintColor = .white
+        cell.likeButton.setImage(UIImage(systemName: row.like ?? false ? "heart.fill" : "heart"), for: .normal)
     }
 }
