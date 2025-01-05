b7ccff99ab6f34992450a5797b6e70fff995898e
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-05 15:20:16 +0900

    Feat: 쇼핑 셀 연결 및 오토레이아웃, headerUI 디자인

---
+++ TravelProject/ThirdTab/ShoppingTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ShoppingTableViewCell.swift
+//  TravelProject
+//
+//  Created by Sebin Kwon on 1/5/25.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    @IBOutlet var cellBackground: UIView!
+    @IBOutlet var checkButton: UIButton!
+    @IBOutlet var favoriteButton: UIButton!
+    @IBOutlet var shoppingLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TravelProject/ThirdTab/ShoppingTableViewController.swift
+++ TravelProject/ThirdTab/ShoppingTableViewController.swift
@@ -9,81 +9,48 @@ import UIKit
 
 class ShoppingTableViewController: UITableViewController {
 
+    @IBOutlet var navigationBar: UINavigationItem!
+    @IBOutlet var textFieldBackground: UIView!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var addButton: UIButton!
+    
+//    var shoppingList = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        configureHeaderUI()
+        configureCellUI()
+        tableView.rowHeight = 65
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath)
+        guard let shoppingCell = cell as? ShoppingTableViewCell else { return cell }
+//        shoppingCell.favoriteButton.tintColor = .black
+        
+        return shoppingCell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func configureHeaderUI() {
+        navigationBar.title = "쇼핑"
+        textFieldBackground.backgroundColor = .systemGray6
+        textFieldBackground.layer.cornerRadius = 10
+        textField.borderStyle = .none
+        textField.placeholder = "무엇을 구매하실 건가요?"
+        textField.backgroundColor = .clear
+        addButton.setTitle("추가", for: .normal)
+        addButton.setTitleColor(.black, for: .normal)
+        addButton.setTitleColor(.darkGray, for: .highlighted)
+        addButton.backgroundColor = .systemGray5
+        addButton.layer.cornerRadius = 10
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func configureCellUI() {
+//        favoritesButton
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
