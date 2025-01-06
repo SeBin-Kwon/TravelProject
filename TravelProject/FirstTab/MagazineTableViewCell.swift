b603869956f36db37a8ed724e2a21de9dbca8cde
SeBin-Kwon <sbkwon98@gmail.com>
2025-01-06 19:20:17 +0900

    Refactor: Magazine 테이블 뷰와 셀 함수 분리, 접근제어

--- TravelProject/FirstTab/MagazineTableViewCell.swift
+++ TravelProject/FirstTab/MagazineTableViewCell.swift
@@ -6,16 +6,47 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MagazineTableViewCell: UITableViewCell {
-
-    @IBOutlet var photoImageView: UIImageView!
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var subTitleLabel: UILabel!
-    @IBOutlet var dateLabel: UILabel!
+    
+    static let identifier = "MagazineTableViewCell"
+    
+    @IBOutlet private var photoImageView: UIImageView!
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var subTitleLabel: UILabel!
+    @IBOutlet private var dateLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        photoImageView.contentMode = .scaleAspectFill
+        photoImageView.layer.cornerRadius = 15
+        titleLabel.numberOfLines = 0
+        titleLabel.font = .systemFont(ofSize: 22, weight: .bold)
+        subTitleLabel.font = .systemFont(ofSize: 15, weight: .medium)
+        subTitleLabel.textColor = .gray
+        dateLabel.font = .systemFont(ofSize: 13)
+        dateLabel.textColor = .gray
+    }
+    
+    func configureData(row: Magazine) {
+        guard let url = URL(string: row.photo_image) else { return }
+        photoImageView.kf.setImage(with: url)
+        titleLabel.text = row.title
+        subTitleLabel.text = row.subtitle
+        dateLabel.text = dateFormatted(row)
+    }
+    
+    private func dateFormatted(_ row: Magazine) -> String {
+        let format = DateFormatter()
+        format.dateFormat = "yyMMdd"
+        let date = format.date(from: row.date)
+        format.dateFormat = "yy년 MM월 dd일"
+        return format.string(from: date ?? Date())
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- TravelProject/FirstTab/MagazineTableViewController.swift
+++ TravelProject/FirstTab/MagazineTableViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 class MagazineTableViewController: UITableViewController {
 
@@ -23,33 +22,12 @@ class MagazineTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: MagazineTableViewCell.identifier, for: indexPath)
         let magazineCell = cell as? MagazineTableViewCell
         guard let magazineCell else { return cell }
-        let magazine = magazines[indexPath.row]
-        
-        guard let url = URL(string: magazine.photo_image) else { return cell }
-        magazineCell.photoImageView.kf.setImage(with: url)
-        magazineCell.photoImageView.contentMode = .scaleAspectFill
-        magazineCell.photoImageView.layer.cornerRadius = 15
-        
-        magazineCell.titleLabel.text = magazine.title
-        magazineCell.titleLabel.numberOfLines = 0
-        magazineCell.titleLabel.font = .systemFont(ofSize: 22, weight: .bold) //1513
-        
-        magazineCell.subTitleLabel.text = magazine.subtitle
-        magazineCell.subTitleLabel.font = .systemFont(ofSize: 15, weight: .medium)
-        magazineCell.subTitleLabel.textColor = .gray
-        
-        let format = DateFormatter()
-        format.dateFormat = "yyMMdd"
-        let date = format.date(from: magazine.date)
-        format.dateFormat = "yy년 MM월 dd일"
-        magazineCell.dateLabel.text = format.string(from: date ?? Date())
-        magazineCell.dateLabel.font = .systemFont(ofSize: 13)
-        magazineCell.dateLabel.textColor = .gray
-        
-        return cell
+        let row = magazines[indexPath.row]
+        magazineCell.configureData(row: row)
+        return magazineCell
     }
 
 }
